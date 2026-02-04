86c424f1972b9196b045f8d23815f119a54c5e33
EMANUELLE CRISTINA HOSTIN <emanuelle_hostin@estudante.sesisenai.org.br>
2026-02-04 14:42:22 -0300

    Feat: Primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// <img id="imagem" src="" alt="Imagem de teste">
+
+// const imagem = document.getElementById('imagem');
+// imagem.setAttribute('src', 'imagem.jpg'); // Adiciona no src
+// imagem.setAttribute('alt', 'Nova descrição'); // Altera o alt
+
+// Isso é para alterar elementos
